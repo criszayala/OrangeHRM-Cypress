6bd117156ab545415b38ae427a6e0aef47c6ba2f
cristian ayala <cristian.adrian1993@gmail.com>
2023-03-09 23:14:48 -0300

    Update spct 'mytest' with positive and negative test related to website title

--- cypress/e2e/mytest.cy.js
+++ cypress/e2e/mytest.cy.js
@@ -1,19 +1,19 @@
 describe('My first test', () => {
-  it('Test1', () => {
+  it('Test1-verify tittle-possitive', () => {
 
     //steps
     //Se ingresa al sitio web
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-
-    //Verificamos el título de la página
-    cy.title().should('eq','OrangeHRM')
-
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    //Verificamos el título de la página utilizando el título actual
+      cy.title().should('eq','OrangeHRM')
   })
-/*
-  it('Test2', () => {
 
-    //stecps
-    cy.visit('https://example.cypress.io')
-  })*/
+  it('Test2-verify tittle-negative', () => {
 
+    //Se ingresa al sitio web
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    //Verificamos el título de la página utilizando título anterior
+      cy.title().should('eq','Orange')
+
+    })
 })
