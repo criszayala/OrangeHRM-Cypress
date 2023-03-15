327ac9ac34070134c57f6837b4ad9cffcb1a36a8
cristian ayala <cristian.adrian1993@gmail.com>
2023-03-15 00:32:49 -0300

    Test 2 is created in 'Login.cy.js' - User login controls - Empty fields

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -4,7 +4,8 @@ describe('User login', () => {
   
       //Se ingresa al sitio web
       cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-      
+      cy.reload(true)
+
       //Se borra posible información existente en el campo 'Username'
       cy.get("[name='username']").clear()
       //Se ingresa el nombre de usuario 'Admin'
@@ -30,10 +31,40 @@ describe('User login', () => {
 
       //Se verifica el cierre de sesión limpiando los campos del formulario de login 'Username'y 'Password'
       cy.get("[name='username']").clear()
-      cy.get("[name='password']").clear()
-      
+            
       //Se verifica que al actualizar el sitio web no se mantiene la sesión del usuario
       cy.reload(true)
 
       })
+
+      it('Test2 - User login controls - Empty fields', () => {
+  
+        //Se ingresa al sitio web
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        
+        //Se borra posible información existente en el campo 'Username'
+        cy.get("[name='username']").clear()
+
+        //Se ingresa contraseña en el campo 'Password'
+        cy.get("[name='password']").type('admin1234')
+
+        //Se hace click en el botón 'Login'
+        cy.get(".oxd-button").click()
+
+        //Se valida que se despliegue mensaje de error en el campo 'Username'
+        cy.get(".oxd-text").contains("Required") 
+
+        //Se ingresa usuario en el campo 'Username'
+        cy.get("[name='username']").type('Admin')
+
+        //Se elimina la contraseña ingresada
+        cy.get("[name='password']").clear()
+
+        //Se hace click en el botón 'Login'
+        cy.get(".oxd-button").click()
+
+        //Se valida que se despliegue mensaje de error en el campo 'Password'
+        cy.get(".oxd-text").contains("Required") 
+        
+        })
   })
