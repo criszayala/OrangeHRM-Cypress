95865970ef0daaa4f18c482b1acc634bb9a7d257
cristian ayala <cristian.adrian1993@gmail.com>
2023-04-10 01:06:51 -0300

    'Should/and' test assertions added

---
+++ cypress/e2e/Assertions.cy.js
@@ -0,0 +1,18 @@
+const { describe } = require('mocha');
+
+describe("Assertions test", ()=>{
+
+        it('Implicit assetions' ,()=>{
+
+            cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+            /*Validaciones del login de usuario*/
+
+            /*1 - Validamos que la url del login incluya valor esperado| Palabras claves: should - and*/
+            cy.url().should('include', 'orangehrmlive.com')
+
+            /*2 - Validamos que la url del login sea la correcta*/
+            cy.url().should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+        })
+})
