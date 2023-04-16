317e6b79758d719e1179b3579f06370f1610fc1b
cristian ayala <cristian.adrian1993@gmail.com>
2023-04-16 01:43:30 -0300

    Added to test case 2 validation for the checkbox (visible element, selection and deselection of an option)

--- cypress/e2e/RadioButtons-Checkbox.cy.js
+++ cypress/e2e/RadioButtons-Checkbox.cy.js
@@ -1,9 +1,11 @@
 describe("Check UI Elements", ()=>{
 
-    it("Checking Radio Buttons",()=>{
+    it("Test 1 - Checking Radio Buttons",()=>{
+
+        //Ingresamos al sitio de prueba
+        cy.visit("https://itera-qa.azurewebsites.net/home/automation")
 
         //1. Se valida que las opciones del radio button estén visibles
-        cy.visit("https://itera-qa.azurewebsites.net/home/automation")
         cy.get("input#male").should('be.visible')
         cy.get("input#female").should('be.visible')
 
@@ -15,4 +17,20 @@ describe("Check UI Elements", ()=>{
         cy.get("input#female").check().should('be.checked')
         cy.get("input#male").should('not.be.checked')
     })
+
+    it("Test 2 - Checking Checkbox",()=>{
+
+        //Ingresamos al sitio de prueba
+        cy.visit("https://itera-qa.azurewebsites.net/home/automation")
+
+        //1. Se valida que el Checkbox esté visible
+        cy.get("input#monday").should('be.visible')
+
+        //2. Se valida que seleccionamos una opción del checkbox (Monday)
+        cy.get("input#monday").check().should('be.checked')
+        
+        //3. Se valida que deseleccionamos opción del checkbox (Monday)
+        cy.get("input#monday").uncheck().should('not.be.checked')
+    })
+
 })
