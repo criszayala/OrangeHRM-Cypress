299ecfb570200f715af3852472050d06a47e9ee4
cristian ayala <cristian.adrian1993@gmail.com>
2023-04-18 00:36:26 -0300

    The drop down country selection is updated (italy //Italy)

--- cypress/e2e/DropDowns.cy.js
+++ cypress/e2e/DropDowns.cy.js
@@ -5,7 +5,7 @@ describe('handle dropdowns',()=>{
 
         //1 - Se selecciona opción específica del DropDown y se valida que la selección sea correcta
         cy.get('#zcf_address_country')
-        .select('italy')
+        .select('Italy')
         .should('have.value', 'Italy')
     })
 })
