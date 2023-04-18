6ef2a56a649c613c7d247f9fcb722f04f6ef4cbc
cristian ayala <cristian.adrian1993@gmail.com>
2023-04-18 00:32:32 -0300

    Specific selection of the 'Drop Down' and validation of the selected option

---
+++ cypress/e2e/DropDowns.cy.js
@@ -0,0 +1,11 @@
+describe('handle dropdowns',()=>{
+
+    it('dropdown with select', ()=>{
+        cy.visit("https://www.zoho.com/commerce/free-demo.html")
+
+        //1 - Se selecciona opción específica del DropDown y se valida que la selección sea correcta
+        cy.get('#zcf_address_country')
+        .select('italy')
+        .should('have.value', 'Italy')
+    })
+})
