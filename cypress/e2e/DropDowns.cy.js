6928cd03cb2594d9d49902bd133c8789a4b3a7d2
cristian ayala <cristian.adrian1993@gmail.com>
2023-04-18 23:54:13 -0300

    Test case for new site, selection and verification of DropDown option

--- cypress/e2e/DropDowns.cy.js
+++ cypress/e2e/DropDowns.cy.js
@@ -8,4 +8,14 @@ describe('handle dropdowns',()=>{
         .select('Italy')
         .should('have.value', 'Italy')
     })
+
+    it('dropdown without select', ()=>{
+        cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
+
+        //2 - Se selecciona opción específica del DropDown y se valida que la selección sea correcta
+        cy.get('#select2-billing_country-container').click()
+        cy.get('.select2-search__field').type('Italy').type('{enter}')
+        cy.get('#select2-billing_country-container')
+        .should('have.tet', 'Italy')
+    })
 })
