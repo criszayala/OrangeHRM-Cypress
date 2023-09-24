ae57c70754daad624e8b98d966d6cebc0402f8d2
cristian ayala <cristian.adrian1993@gmail.com>
2023-09-24 01:36:30 -0300

    unchecked box assertion is updated

--- cypress/e2e/RadioButtons.cy.js
+++ cypress/e2e/RadioButtons.cy.js
@@ -10,13 +10,13 @@ describe("Check UI elements",()=>{
 
         //Selection radio buttons + validación de única selección (R. Button 1)
         cy.get("input#vfb-7-1").check().should('be.checked') //marcamos radio button y verificamos la selección
-        cy.get("input#vfb-7-2").should('not.be.visible')  //Se verifica que NO esté seleccionado el radio button 2
-        cy.get("input#vfb-7-3").should('not.be.visible') //Se verifica que NO esté seleccionado el radio button 3
+        cy.get("input#vfb-7-2").should('not.be.checked')  //Se verifica que NO esté seleccionado el radio button 2
+        cy.get("input#vfb-7-3").should('not.be.checked') //Se verifica que NO esté seleccionado el radio button 3
    
         //Selection radio buttons + validación de única selección (R. Button 3)
         cy.get("input#vfb-7-3").check().should('be.checked') //marcamos radio button y verificamos la selección
-        cy.get("input#vfb-7-1").should('not.be.visible')  //Se verifica que NO esté seleccionado el radio button 1
-        cy.get("input#vfb-7-2").should('not.be.visible') //Se verifica que NO esté seleccionado el radio button 2
+        cy.get("input#vfb-7-1").should('not.be.checked')  //Se verifica que NO esté seleccionado el radio button 1
+        cy.get("input#vfb-7-2").should('not.be.checked') //Se verifica que NO esté seleccionado el radio button 2
    
    
     })
