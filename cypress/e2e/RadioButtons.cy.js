cdb7d9c1244a18f3eb112a66655a3446acd5dfe8
cristian ayala <cristian.adrian1993@gmail.com>
2023-09-24 01:19:10 -0300

    radio button selection and selection validation in others + Cypress update

--- cypress/e2e/RadioButtons.cy.js
+++ cypress/e2e/RadioButtons.cy.js
@@ -3,11 +3,15 @@ describe("Check UI elements",()=>{
     it("Checking Radio Buttons",()=>{
         cy.visit("https://demo.guru99.com/test/radio.html")
             
-        //búsqueda de un elemento por ID + condición (función que comprueba elemento presente en la página)
+        //búsqueda de un elemento por ID + condición (función que comprueba elemento esté visible en la página)
         cy.get("input#vfb-7-1").should('be.visible') //Radio 1
         cy.get("input#vfb-7-2").should('be.visible') //Radio 2
         cy.get("input#vfb-7-3").should('be.visible') //Radio 3
 
-        cy.window().then(win => win.close()); // Cierra la ventana actual
+        //Selection radio buttons
+        cy.get("input#vfb-7-1").check().should('be.checked') //marcamos radio button y verificamos la selección
+       
+        cy.get("input#vfb-7-2").should('not.be.visible')  //Se verifica que NO esté seleccionado el radio button 2
+        cy.get("input#vfb-7-3").should('not.be.visible') //Se verifica que NO esté seleccionado el radio button 3
     })
 })
