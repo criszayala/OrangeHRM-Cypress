28b9ef4ed8e836cfacb2a489d8dc0a45f74c549b
cristian ayala <cristian.adrian1993@gmail.com>
2023-09-24 01:24:14 -0300

    new radio button selection and single selection check

--- cypress/e2e/RadioButtons.cy.js
+++ cypress/e2e/RadioButtons.cy.js
@@ -8,10 +8,16 @@ describe("Check UI elements",()=>{
         cy.get("input#vfb-7-2").should('be.visible') //Radio 2
         cy.get("input#vfb-7-3").should('be.visible') //Radio 3
 
-        //Selection radio buttons
+        //Selection radio buttons + validación de única selección (R. Button 1)
         cy.get("input#vfb-7-1").check().should('be.checked') //marcamos radio button y verificamos la selección
-       
         cy.get("input#vfb-7-2").should('not.be.visible')  //Se verifica que NO esté seleccionado el radio button 2
         cy.get("input#vfb-7-3").should('not.be.visible') //Se verifica que NO esté seleccionado el radio button 3
+   
+        //Selection radio buttons + validación de única selección (R. Button 3)
+        cy.get("input#vfb-7-3").check().should('be.checked') //marcamos radio button y verificamos la selección
+        cy.get("input#vfb-7-1").should('not.be.visible')  //Se verifica que NO esté seleccionado el radio button 1
+        cy.get("input#vfb-7-2").should('not.be.visible') //Se verifica que NO esté seleccionado el radio button 2
+   
+   
     })
 })
