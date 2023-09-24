240e96e93bd1e739f5afbc55de52957557d5f8ec
cristian ayala <cristian.adrian1993@gmail.com>
2023-09-24 00:37:26 -0300

    Test case update, new url and validation of 3 radio buttons (Visibility)

--- cypress/e2e/RadioButtons.cy.js
+++ cypress/e2e/RadioButtons.cy.js
@@ -1,12 +1,13 @@
-describe("Check UI elements"),()=>{
+describe("Check UI elements",()=>{
 
-        it("Checking Radio Buttons",()=>{
-            cy.visit("https://artoftesting.com/samplesiteforselenium")
+    it("Checking Radio Buttons",()=>{
+        cy.visit("https://demo.guru99.com/test/radio.html")
             
-        //búsqueda de un elemento por ID en la web + condición (función que comprueba elemento presente en la página)
-            cy.get("input#male").should(be.visible)
-            cy.get("input#male").should(be.visible)
-        })
+        //búsqueda de un elemento por ID + condición (función que comprueba elemento presente en la página)
+        cy.get("input#vfb-7-1").should('be.visible') //Radio 1
+        cy.get("input#vfb-7-2").should('be.visible') //Radio 2
+        cy.get("input#vfb-7-3").should('be.visible') //Radio 3
 
-
-}
+        cy.window().then(win => win.close()); // Cierra la ventana actual
+    })
+})
